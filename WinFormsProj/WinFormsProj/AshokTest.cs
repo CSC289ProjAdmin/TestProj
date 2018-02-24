9cfd5e4bc78a2f6e7c16d9e43fa76cc2c492b095
ASHOK\Ashok <ashokmnr160@gmail.com>
2018-02-24 13:58:10 -0500

    AShokNewDummy

---
+++ WinFormsProj/WinFormsProj/AshokTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{
+    class AshokTest
+    {
+        public static string GetDummyString()
+        {
+            return "dummy";
+        }
+    }
+}
