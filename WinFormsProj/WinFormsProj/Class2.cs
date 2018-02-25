1060ad7155f4e88d9eecafdeba5f858dca01e59a
Chris Godwin <csgodwin2@my.waketech.edu>
2018-02-25 18:56:18 -0500

    Test2 class

---
+++ WinFormsProj/WinFormsProj/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{   //added class
+    public class Class2
+    {
+        public static string Test2
+        {
+            get
+            {
+                return "Test2";
+            }
+        }
+    }
+}
--- WinFormsProj/WinFormsTest/UnitTest1.cs
+++ WinFormsProj/WinFormsTest/UnitTest1.cs
@@ -20,4 +20,34 @@ namespace WinFormsTest {
             Assert.AreEqual(testStr, "Dummy");
         }
     }
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string testStr = WinFormsProj.Class2.Test2;
+            Assert.AreEqual(testStr, "Test2");
+        }
+    }
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string testStr = WinFormsProj.Class3.Test3;
+            Assert.AreEqual(testStr, "Test3");
+        }
+    }
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string testStr = WinFormsProj.Class3.Test4;
+            Assert.AreEqual(testStr, "Test4");
+        }
+    }
 }
