6a2fb80283cb8ac2999226f220b235bdba531730
Chris Godwin <csgodwin2@my.waketech.edu>
2018-02-25 18:59:19 -0500

    Test4

---
+++ WinFormsProj/WinFormsProj/Class4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{   //added class
+    public class Class4
+    {
+        public static string Test4
+        {
+            get
+            {
+                return "Test4";
+            }
+        }
+    }
+}
