66a44efd5b0d3a522f0bfd12918bf711bcf64c84
Chris Godwin <csgodwin2@my.waketech.edu>
2018-02-25 18:57:51 -0500

    Test3

---
+++ WinFormsProj/WinFormsProj/Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{   //added class
+        public class Class3
+        {
+            public static string Test3
+            {
+                get
+                {
+                    return "Test3";
+                }
+            }
+        }
+}
