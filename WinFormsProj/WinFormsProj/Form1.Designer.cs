3fabbbeaa176d3231e5d3549b48dd638739abab7
Ryan <fake@fake.fake>
2018-02-26 12:24:40 -0500

    testing db

--- WinFormsProj/WinFormsProj/Form1.Designer.cs
+++ WinFormsProj/WinFormsProj/Form1.Designer.cs
@@ -23,12 +23,34 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.lblDbTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDbTest
+            // 
+            this.lblDbTest.AutoSize = true;
+            this.lblDbTest.Location = new System.Drawing.Point(26, 78);
+            this.lblDbTest.Name = "lblDbTest";
+            this.lblDbTest.Size = new System.Drawing.Size(35, 13);
+            this.lblDbTest.TabIndex = 0;
+            this.lblDbTest.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblDbTest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDbTest;
     }
 }
 
--- WinFormsProj/WinFormsProj/Form1.cs
+++ WinFormsProj/WinFormsProj/Form1.cs
@@ -7,11 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WinFormsProj {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            string connStr = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=WinFormsDb;Integrated Security=True;Pooling=False;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connStr);
+            string query = "Select * From TestTable";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.CommandType = CommandType.Text;
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            rdr.Read(); // first line
+            string text = $"{rdr["Amount"]:c}";
+            
+            rdr.Close();
+
+            lblDbTest.Text = text;
         }
     }
 }
