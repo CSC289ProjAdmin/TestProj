2b385467461b8021df661182293ce02dc4d4507f
jeunderwood <jodi.underwood1027@gmail.com>
2018-02-27 16:48:11 -0500

    staging class test and built class 1

---
+++ WinFormsProj/WinFormsProj/JodiClass1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{
+    public class JodiClass1
+    {
+        public static int jodi(int a, int b, int c)
+        {
+            int d = a + b + c;
+            return d;
+        }
+    }
+}
