0debb54e908170fa5fa9440379331d4012457711
ASHOK\Ashok <ashokmnr160@gmail.com>
2018-02-27 18:00:27 -0500

    AshokNewTest_Branch

---
+++ WinFormsProj/WinFormsProj/AshokNewClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{
+
+    public class AshokNewClass
+    {
+        public static string GetString()
+        {
+            return "Ashok";
+        }
+    }
+}
---
+++ WinFormsProj/WinFormsTest/AshokNewClassTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTest
+{
+    [TestClass]
+    public class AshokNewClassTest
+    {
+        [TestMethod]
+        public void Method2()
+        {
+            string test = WinFormsProj.AshokNewClass.GetString();
+            Assert.AreEqual(test, "Ashok");
+        }
+    }
+}
