cb6a74471ede2b4df71423e380aad592ceafa44c
ASHOK\Ashok <ashokmnr160@gmail.com>
2018-02-27 16:31:03 -0500

    AshokTest_Branch2

---
+++ WinFormsProj/WinFormsProj/AshokMethod2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{
+    public class AshokMethod2
+    {
+        public static decimal GetDecimal()
+        {
+            return 5.5M;
+        }
+    }
+}
