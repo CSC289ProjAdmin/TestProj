38b8c0994458fbd70e335a73b485db3f19e0fe7f
jeunderwood <jodi.underwood1027@gmail.com>
2018-02-27 17:10:36 -0500

    committing written unit tests from winFormsTest

---
+++ WinFormsProj/WinFormsTest/JodiClass1Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinFormsTest
+{
+    [TestClass]
+    public class JodiClass1Test
+    {
+        [TestMethod]
+
+        public void JodiTest1()
+        {
+            int actual = WinFormsProj.JodiClass1.jodi(1, 2, 3);
+            int expected = 1 + 2 + 3;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- WinFormsProj/WinFormsTest/UnitTest1.cs
+++ WinFormsProj/WinFormsTest/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace WinFormsTest {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+            public void TestMethod1() {
             string testStr = WinFormsProj.Dummy.GetDummyString();
             Assert.AreEqual(testStr, "dummy");
         }
