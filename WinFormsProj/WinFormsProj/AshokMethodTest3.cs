805cffc816faaf7cfc066687ff879db3f9ae1b7d
ASHOK\Ashok <ashokmnr160@gmail.com>
2018-02-27 16:53:23 -0500

    AshokTest_Branch3

---
+++ WinFormsProj/WinFormsProj/AshokMethodTest3.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsProj
+{
+    [TestClass]
+    public class AshokMethodTest3
+    {
+        [TestMethod]
+        public void Method3()
+        {
+            string lastname = WinFormsProj.AshokMethod3.GetLastN();
+            Assert.AreEqual(lastname, "Thakur");
+        }
+    }
+}
